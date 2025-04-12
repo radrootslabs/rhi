93941fa77667bd1ee3d74b341e4cb100fa10a38b
triesap <137732411+triesap@users.noreply.github.com>
2025-04-12 18:40:42 +0000

    Adds KeyProfile to manage signing keys, profile metadata, and NIP-89 descriptor metadata.

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod keys;
+
+pub const KIND_JOB_REQUEST: u16 = 5300;
+pub const KIND_JOB_RESPONSE: u16 = 6300;
+pub const KIND_APPLICATION_HANDLER: u16 = 31990;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use anyhow::Result;
 use clap::Parser;
-use nostr::{Filter, Keys, Kind, Timestamp};
+use nostr::{Filter, Keys, Kind, Timestamp, event::Event, nips::nip01::Metadata};
 use nostr_sdk::{Client, RelayPoolNotification};
+use rhi::{KIND_JOB_REQUEST, keys::KeyProfile};
 use tokio::signal::unix::{SignalKind, signal};
 use tracing::{error, info};
+
+struct ConfigMetadata {
+    name: String,
+    nip_05: Option<String>,
+}
 
 fn init_tracing() {
     tracing_subscriber::fmt::init();
 }
 
-async fn subscribe(relays: Vec<String>) -> Result<()> {
-    info!("Subscription started for kind 5300");
-
-    let keys = Keys::generate();
+async fn subscribe(keys: Keys, relays: Vec<String>) -> Result<()> {
     let client = Client::new(keys);
     for relay in relays.iter() {
         client.add_relay(relay).await?;
@@ -20,10 +23,14 @@ async fn subscribe(relays: Vec<String>) -> Result<()> {
     client.connect().await;
 
     let filter = Filter::new()
-        .kind(Kind::Custom(5300))
+        .kind(Kind::Custom(KIND_JOB_REQUEST))
         .since(Timestamp::now());
 
     client.subscribe(filter, None).await?;
+
+    info!("Subscription started for kind {}", {
+        KIND_JOB_REQUEST.to_string()
+    });
 
     let mut notifications = client.notifications();
 
@@ -49,8 +56,32 @@ async fn subscribe(relays: Vec<String>) -> Result<()> {
     version = env!("CARGO_PKG_VERSION")
 )]
 pub struct Args {
-    #[arg(long, help = "Adds nostr relays to the subscription", required = true)]
+    #[arg(long, help = "Adds the keys profiles file path.", required = true)]
+    pub keys: String,
+
+    #[arg(long, help = "Adds nostr relays to the subscription.", required = true)]
     pub relays: Vec<String>,
+
+    #[arg(
+        long,
+        help = "(Optional) Sets flag to generate keys if none are found.",
+        required = false
+    )]
+    pub generate_keys: bool,
+
+    #[arg(
+        long,
+        help = "(Optional) Adds the application handler identifier tag (NIP-89)",
+        required = false
+    )]
+    pub identifier: Option<String>,
+
+    #[arg(
+        long,
+        help = "(Optional) Adds the domain name (NIP-05) to metadata",
+        required = false
+    )]
+    pub nip05_domain: Option<String>,
 }
 
 #[tokio::main]
@@ -62,9 +93,57 @@ async fn main() -> Result<()> {
 
     info!("Starting");
 
+    let mut key_profile = KeyProfile::init(args.keys, args.generate_keys, args.identifier)?;
+
+    let keys = key_profile.keys()?;
+
+    let config = ConfigMetadata {
+        name: "rhi".to_string(),
+        nip_05: args.nip05_domain,
+    };
+
+    let metadata = Metadata {
+        name: Some(config.name.clone()),
+        display_name: None,
+        picture: None,
+        nip05: config
+            .nip_05
+            .as_ref()
+            .map(|domain| format!("{}@{}", config.name, domain)),
+        ..Default::default()
+    };
+
+    let mut events: Vec<Event> = vec![];
+
+    if let Some(event) = key_profile.build_metadata(&metadata).await? {
+        events.push(event);
+    }
+
+    if let Some(event) = key_profile.build_application_handler().await? {
+        events.push(event);
+    }
+
+    if !events.is_empty() {
+        let client = Client::new(keys.clone());
+        for relay in relays.iter() {
+            client.add_relay(relay).await?;
+        }
+        client.connect().await;
+        for event in events {
+            client.send_event(&event).await?;
+            info!("Sent kind {} event for key profile", {
+                event.clone().kind
+            })
+        }
+        client.disconnect().await;
+    }
+
+    let keys_sub = keys.clone();
+    let relays_sub = relays.clone();
+
     tokio::spawn(async move {
         loop {
-            if let Err(e) = subscribe(relays.clone()).await {
+            if let Err(e) = subscribe(keys_sub.clone(), relays_sub.clone()).await {
                 error!("Error on subscription: {e}");
             }
         }
